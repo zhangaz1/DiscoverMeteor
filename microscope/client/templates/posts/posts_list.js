fa8fc2407ad95e5b4c1c3827e2564588b884612f
zhangaz1 <zhangaz1@hotmail.com>
2015-09-08 14:45:21 +0800

    Wired collection into 'postsList' template

--- microscope/client/templates/posts/posts_list.js
+++ microscope/client/templates/posts/posts_list.js
@@ -1,14 +1,3 @@
-var postsData = [{
-    title: 'Introduceing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-}, {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-}, {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-}];
-
 Template.postsList.helpers({
-    posts: postsData
+    posts: Posts.find()
 });
