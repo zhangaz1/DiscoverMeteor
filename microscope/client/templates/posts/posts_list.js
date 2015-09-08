8368753681d7abb54c35f8a5f82a446fe89dd894
zhangaz1 <zhangaz1@hotmail.com>
2015-09-08 10:50:24 +0800

    Added basic posts list template and static data.

---
+++ microscope/client/templates/posts/posts_list.js
@@ -0,0 +1,14 @@
+var postsData = [{
+    title: 'Introduceing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+}, {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+}, {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+}];
+
+Template.postsList.helpers({
+    posts: postsData
+});
