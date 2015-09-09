74258ba3095a3253e0272b666aac151377566e9c
zhangaz1 <zhangaz1@hotmail.com>
2015-09-09 17:38:26 +0800

    Added edit posts form

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -18,6 +18,13 @@ Router.route('/posts/:_id', {
     }
 });
 
+Router.route('/posts/:_id/edit', {
+    name: 'postEdit',
+    data: function() {
+        return Posts.findOne(this.params._id);
+    }
+})
+
 Router.route('/submit', {
     name: 'postSubmit'
 });
