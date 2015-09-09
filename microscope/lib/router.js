924952f516b8caeb1546e2848f4ed1c443cc1cb3
zhangaz1 <zhangaz1@hotmail.com>
2015-09-09 09:31:47 +0800

    Added not found template

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -1,6 +1,7 @@
 Router.configure({
     layoutTemplate: 'layout',
     loadingTemplate: 'loading',
+    notFoundTemplate: 'notFound',
     waitOn: function() {
         return Meteor.subscribe('posts');
     }
@@ -16,3 +17,7 @@ Router.route('/posts/:_id', {
         return Posts.findOne(this.params._id);
     }
 })
+
+Router.onBeforeAction('dataNotFound', {
+    only: 'postPage'
+});
