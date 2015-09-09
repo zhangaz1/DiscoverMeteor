2e5f05f0198d96f4ff4d33e3c27f38d22e2ff8d1
zhangaz1 <zhangaz1@hotmail.com>
2015-09-09 14:42:35 +0800

    Denied access to new posts page when not logged in

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -25,3 +25,15 @@ Router.route('/submit', {
 Router.onBeforeAction('dataNotFound', {
     only: 'postPage'
 });
+
+var requireLogin = function() {
+    if (!Meteor.user()) {
+        this.render('accessDenied');
+    } else {
+        this.next();
+    }
+};
+
+Router.onBeforeAction(requireLogin, {
+    only: 'postSubmit'
+});
