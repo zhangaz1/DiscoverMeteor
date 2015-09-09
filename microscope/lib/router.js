bdf7ea8996f8aa77610334e8744506f63ab63eeb
zhangaz1 <zhangaz1@hotmail.com>
2015-09-09 09:15:26 +0800

    Routing to a single post page

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -9,3 +9,10 @@ Router.configure({
 Router.route('/', {
     name: 'postsList'
 });
+
+Router.route('/posts/:_id', {
+    name: 'postPage',
+    data: function() {
+        return Posts.findOne(this.params._id);
+    }
+})
